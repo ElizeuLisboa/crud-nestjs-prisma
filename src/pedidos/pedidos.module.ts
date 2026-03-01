798e492a06ccef63b8c4f9ac1f2fcb37693d167f
ElizeuLisboa <eliseu.lisboa@gmail.com>
2026-03-01 17:01:55 -0300

    ajustes para deploy

--- src/pedidos/pedidos.module.ts
+++ src/pedidos/pedidos.module.ts
@@ -3,7 +3,7 @@ import { PedidosService } from "./pedidos.service";
 import { PedidosController } from "./pedidos.controller";
 import { PrismaService } from "../prisma/prisma.service";
 import { PagamentosModule } from "../pagamentos/pagamentos.module"; // <-- importante!
-import { AuthModule } from "@/modules/auth/auth.module";
+import { AuthModule } from "../modules/auth/auth.module";
 
 @Module({
   imports: [PagamentosModule,AuthModule],//adiciona isso
